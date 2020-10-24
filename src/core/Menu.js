b1cd9ebc751378be53b9178722f821b334fbb48c
rigel selmani <rigelrigelselmani@hotmail.com>
2020-10-24 05:55:41 -0400

    manu file and making the links in the menu

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -4,8 +4,19 @@ import {Link,withRouter} from "react-router-dom";
 function Menu(){
     return (
         <div>
+          <ul className="nav nav-tabs bg-primary">
+            <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/signin">SignIn</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/signup">SignUp</Link>
+            </li>
+          </ul>
         </div>
     )
 }
 
-export default Menu;
+export default withRouter(Menu);
